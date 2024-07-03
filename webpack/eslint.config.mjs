b58802c63299fa91fac743559a6a14cf20c53952
jorge.carreno <jorge.carreno@devo.com>
2024-07-03 09:30:12 +0200

    eslint

--- webpack/eslint.config.mjs
+++ webpack/eslint.config.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import globals from 'globals';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
